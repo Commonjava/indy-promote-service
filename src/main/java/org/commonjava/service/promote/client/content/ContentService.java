fda4330ffda96b27b08cadfe251ca4518f115bfe
Rui Han <ruhan@redhat.com>
2022-12-08 13:58:14 +0800

    Fix content service base url

--- src/main/java/org/commonjava/service/promote/client/content/ContentService.java
+++ src/main/java/org/commonjava/service/promote/client/content/ContentService.java
@@ -5,17 +5,19 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
-@Path("/api/content/{package}/{type: (hosted|group|remote)}/{name}/{path: (.*)}")
+@Path("/api/content")
 @RegisterRestClient(configKey="content-service-api")
 public interface ContentService
 {
     @GET
+    @Path("{package}/{type: (hosted|group|remote)}/{name}/{path: (.*)}")
     Response retrieve(final @PathParam( "package" ) String packageName,
                       final @PathParam( "type") String type,
                       final @PathParam( "name") String name,
                       final @PathParam( "path" ) String path);
 
     @HEAD
+    @Path("{package}/{type: (hosted|group|remote)}/{name}/{path: (.*)}")
     Response exists(final @PathParam( "package" ) String packageName,
                     final @PathParam( "type") String type,
                     final @PathParam( "name") String name,
