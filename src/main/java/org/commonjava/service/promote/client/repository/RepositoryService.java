aafa5158fdc83b208527c692e81b7675428ac2e9
Rui Han <ruhan@redhat.com>
2022-12-09 11:45:10 +0800

    Add access tokens to headers when calling other serv

--- src/main/java/org/commonjava/service/promote/client/repository/RepositoryService.java
+++ src/main/java/org/commonjava/service/promote/client/repository/RepositoryService.java
@@ -1,5 +1,7 @@
 package org.commonjava.service.promote.client.repository;
 
+import org.commonjava.service.promote.client.CustomClientRequestFilter;
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.ws.rs.*;
@@ -7,26 +9,11 @@ import javax.ws.rs.core.Response;
 
 @Path("/api/admin/stores")
 @RegisterRestClient(configKey="repo-service-api")
+@RegisterProvider(CustomClientRequestFilter.class)
 public interface RepositoryService
 {
     @GET
     @Path("/{packageType}/{type: (hosted|group|remote)}/{name}")
     Response getStore(@PathParam("packageType") String packageType, @PathParam("type") String type, @PathParam("name") String name);
-
-/*
-    @PUT
-    @Path("/{packageType}/{type: (hosted|group|remote)}/{name}")
-    Response putStore(@PathParam("packageType") String packageType, @PathParam("type") String type, @PathParam("name") String name);
-
-    @POST
-    @Path("/{packageType}/{type: (hosted|group|remote)}")
-    Response create(@PathParam( "packageType" ) String packageType, @PathParam( "type" ) String type);
-*/
-
-/*
-    @GET
-    @Path( "/query/affectedBy" )
-    Response getGroupsAffectedBy( final @QueryParam( "keys" ) String keys );
-*/
 
 }
