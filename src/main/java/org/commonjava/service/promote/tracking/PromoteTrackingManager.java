039b40bb59e945b7586e803aba9141eaf0a6a28a
Rui Han <ruhan@redhat.com>
2023-04-03 12:27:37 +0800

    Add PromoteTrackingRecord and REST endpoint to get by trackingId (#62)

---
+++ src/main/java/org/commonjava/service/promote/tracking/PromoteTrackingManager.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2022 Red Hat, Inc. (https://github.com/Commonjava/service-parent)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.service.promote.tracking;
+
+import org.commonjava.service.promote.model.PromoteTrackingRecord;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Optional;
+
+@ApplicationScoped
+public class PromoteTrackingManager
+{
+    public Optional<PromoteTrackingRecord> getTrackingRecord(String trackingId )
+    {
+        return null; // TODO
+    }
+}
